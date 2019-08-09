e4061555d454dc737fe9e40ff2b6c4338414de91
Sunil Singh <sunilsingh@Sunils-MacBook-Air.local>
2019-08-09 13:06:31 +0530

    Add basic test for question-update using fixture

---
+++ myquora/tests/conftest.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+from django.contrib.auth.models import User
+from myquora.models import Question, Answer, Comment, Author
+import pytest
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def authenticated_user(client,  django_user_model):
+    """Create an authenticated user for test"""
+    user = django_user_model.objects.create_user(username='test', password='test123')
+    user.save()
+    author = Author.objects.create(user=user)
+    author.save()
+    client.login(username='test', password='test123')
+    return (client, author)
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def create_a_question(client, django_user_model):
+    user = django_user_model.objects.create_user(username='test1', password='test1123')
+    author = Author.objects.create(user=user)
+    question = Question.objects.create(author=author, question_text="some question")
+    print('Fixture: create a question')
+    return question
--- myquora/tests/test_views.py
+++ myquora/tests/test_views.py
@@ -5,24 +5,54 @@ from django.contrib.sessions.models import Session
 from myquora.models import Question, Author
 
 
+@pytest.mark.django_db
+def test_question_update_success(authenticated_user, create_a_question):
+    """creating a question"""
+    client, author = authenticated_user
+    question = create_a_question
+    url = urls.reverse('question-update', kwargs={'pk': question.id})
+    response = client.get(url)
+    updated_question = {'question_text': "Newdfdf"}
+    response = client.post(url, updated_question, follow=True)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_redirect_to_login_when_update_question(create_a_question, client, django_user_model):
+    """Verify we redirect to the login page when a user
+       tries to update a new question
+     """
+    question = create_a_question
+    url = urls.reverse('question-update', kwargs={'pk': question.id})
+    resp = client.get(url)
+    assert resp.status_code == 302
+
+
+@pytest.mark.django_db
+def test_redirect_to_login_when_add_question(client, django_user_model):
+    """Verify we redirect to the login page when a user
+       tries to add a new question
+     """
+    url = urls.reverse('question-add')
+    resp = client.get(url)
+    assert resp.status_code == 302
+
+
+@pytest.mark.django_db
+def test_redirect_to_login(create_a_question, client, django_user_model):
+    question = create_a_question
+    url = urls.reverse('question-detail', kwargs={'pk': question.id})
+    response = client.get(url, follow=True)
+    assert b'myquora/author/add/?next=/myquora/question/1' in response.content
+    assert response.context['question'].question_text == question.question_text
+
 
 # @pytest.mark.django_db
-# def test_redirect_to_memes_when_logged_in(authenticated_user, client):
-#     """Verify we redirect to the memes page when a user is logged in"""
-#     url = urls.reverse('answer-update', kwargs={'pk': 2})
-#     resp = client.get(url)
-#     pprint.pprint('Test-running...')
-#     assert resp.status_code == 302
-#     # assert resp.url == urls.reverse('answer-update', kwargs={'pk': 2})
-
-
-# @pytest.mark.django_db
-# def test_redirect_to_marketing_when_logged_out(client):
+# def test_redirect_to_login_when_logged_out_from_update_question(client):
 #     """Verify we redirect to the marketing page when a user is not logged in"""
 #     question = Question(fill waith)
 #     question.save()
 
-    
 
 #     url = urls.reverse('answer-update', kwargs={'pk': 2})
 #     resp = client.get(url)
@@ -30,15 +60,3 @@ from myquora.models import Question, Author
 #     # pprint.pprint(resp.__dict__, indent=5)
 #     assert resp.status_code == 302
 #     # assert b'answer List' in resp.content
-
-
-@pytest.mark.django_db
-def test_redirect_to_login(client, django_user_model):
-    user = django_user_model.objects.create_user(username='test', password='test123')
-    author = Author.objects.create(user=user)
-    question = Question.objects.create(author=author, question_text="some question")
-    url = urls.reverse('question-detail', kwargs={'pk': question.id})
-    response = client.get(url, follow=True)
-    print(dir(response))
-    print(response.status_code)
-    assert response.context['question'].question_text == question.question_text
