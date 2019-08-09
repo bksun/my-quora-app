974ffe019382d8472313ef7b061aa1ce39d7e5c1
Sunil Singh <sunilsingh@Sunils-MacBook-Air.local>
2019-08-09 15:03:23 +0530

    Add redirect test on logout from secured links like question-update

--- myquora/tests/test_views.py
+++ myquora/tests/test_views.py
@@ -11,10 +11,23 @@ def test_question_update_success(authenticated_user, create_a_question):
     client, author = authenticated_user
     question = create_a_question
     url = urls.reverse('question-update', kwargs={'pk': question.id})
-    response = client.get(url)
     updated_question = {'question_text': "Newdfdf"}
     response = client.post(url, updated_question, follow=True)
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_redirect_to_login_when_logged_out_from_update_question(
+  authenticated_user, create_a_question):
+    """Verify we redirect to the login page when a user is logged out"""
+    client, author = authenticated_user
+    question = create_a_question
+    url = urls.reverse('question-update', kwargs={'pk': question.id})
+    client.logout()
+    resp = client.get(url)
+    assert 'login' in resp.url
+    assert 'next' in resp.url
+    assert resp.status_code == 302
 
 
 @pytest.mark.django_db
@@ -45,18 +58,3 @@ def test_redirect_to_login(create_a_question, client, django_user_model):
     response = client.get(url, follow=True)
     assert b'myquora/author/add/?next=/myquora/question/1' in response.content
     assert response.context['question'].question_text == question.question_text
-
-
-# @pytest.mark.django_db
-# def test_redirect_to_login_when_logged_out_from_update_question(client):
-#     """Verify we redirect to the marketing page when a user is not logged in"""
-#     question = Question(fill waith)
-#     question.save()
-
-
-#     url = urls.reverse('answer-update', kwargs={'pk': 2})
-#     resp = client.get(url)
-#     print('*************************************')
-#     # pprint.pprint(resp.__dict__, indent=5)
-#     assert resp.status_code == 302
-#     # assert b'answer List' in resp.content
