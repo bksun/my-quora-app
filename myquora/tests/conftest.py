96bd6d7163afa1f2a2e06b341b89c6b9fe6405d0
Sunil Singh <sunilsingh@Sunils-MacBook-Air.local>
2019-08-12 14:00:11 +0530

    Add tests for answer-add

--- myquora/tests/conftest.py
+++ myquora/tests/conftest.py
@@ -16,8 +16,16 @@ def authenticated_user(client, django_user_model):
 
 @pytest.fixture
 @pytest.mark.django_db
-def create_a_question(client, django_user_model):
-    user = django_user_model.objects.create_user(username='test1', password='test1123')
-    author = Author.objects.create(user=user)
+def create_a_question(authenticated_user, client):
+    client, author = authenticated_user
     question = Question.objects.create(author=author, question_text="some question")
     return question
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def create_an_answer(authenticated_user, create_a_question, client):
+    client, author = authenticated_user
+    question = create_a_question
+    answer = Answer.objects.create(author=author, question=question, answer_text="some answer")
+    return answer
